0f765b2b3d3b57a417686dd167151f7cf8875687
Fellipe Calleia <fellipe@calleia.com.br>
2019-11-16 18:02:05 -0300

    Add command line arguments

--- main.c
+++ main.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char** argv) {
+    long int height, width;
+    
+    if (argc == 3) {
+        width = strtol(argv[1], NULL, 10);
+        height = strtol(argv[2], NULL, 10);
+        
+        // TODO: call make random image function
+    } else {
+        fprintf(stderr, "Invalid number of arguments.");
+    }
+    
 	return 0;
 }
